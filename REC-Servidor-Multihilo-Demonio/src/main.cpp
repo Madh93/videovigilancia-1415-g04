9ba0b1c366090701d0dac0b83da03ad2fcf7dea9
Madh93 <michelangelo93@gmail.com>
2015-05-31 22:21:57 +0100

    optimizando la demonizacion

--- REC-Servidor-Multihilo-Demonio/src/main.cpp
+++ REC-Servidor-Multihilo-Demonio/src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QDebug>
 #include "rec.hpp"
 #include <unistd.h> //fork(), close(), setsid()...
 #include <sys/stat.h> //umask()
@@ -31,14 +32,14 @@ int main(int argc, char *argv[]) {
     }
 
     // Si pid es > 0, estamos en el proceso padre
-    if (pid > 0){
+    if (pid > 0)
         exit(0); // Terminar el proceso
-    }
+
     // Si la ejecución llega a este punto, estamos en el hijo
 
-    /******************/
-    /* Cambiar Umask */
-    /****************/
+    /*********************/
+    /* Cambiar Permisos */
+    /*******************/
     umask(0);
 
     /************************/
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]) {
     openlog(argv[0], LOG_NOWAIT | LOG_PID, LOG_USER);
 
     // Enviar un mensaje al demonio syslog
-    syslog(LOG_NOTICE, "Demonio iniciado con éxito");
+    syslog(LOG_NOTICE, "Demonio REC iniciado con éxito");
 
 
     /***********************/
@@ -65,40 +66,44 @@ int main(int argc, char *argv[]) {
 
     // Cambiar directorio de trabajo
     if ((chdir("/")) < 0) {
-        syslog(LOG_ERR, "No fue posible cambiar el directorio de trabajo a /\n");
+        syslog(LOG_ERR, "No fue posible cambiar el directorio de trabajo a '/'\n");
         exit(12);
     }
 
+    /***************************/
+    /* Redirigir descriptores */
     /*************************/
-    /* Reabrir descriptores */
-    /***********************/
 
     // Cerrar los descriptores de la E/S estándar
     close(STDIN_FILENO);            // fd 0
     close(STDOUT_FILENO);           // fd 1
-    //close(STDERR_FILENO);           // fd 2     //produce segfault
+    close(STDERR_FILENO);           // fd 2
 
     // Abrir nuevos descriptores de E/S
     int fd0 = open("/dev/null", O_RDONLY);  // fd0 == 0
     int fd1 = open("/dev/null", O_WRONLY);  // fd0 == 1
-    //int fd2 = open("/dev/null", O_WRONLY);  // fd0 == 2 produce segfault
+    int fd2 = open("/dev/null", O_WRONLY);  // fd0 == 2
 
 
     /*************************/
     /* Cambiar personalidad */
     /***********************/
 
-    // Cambiar el usuario efectivo del proceso a 'recd'
-    passwd* user = getpwnam("recd");
-    seteuid(user->pw_uid);
+    // Cambiar el usuario efectivo del proceso
+    passwd* usuario = getpwnam("migue");   // USUARIO PROVISIONAL!
+    if (!usuario) {
+        std::cerr << "No existe el usuario en el sistema." << std::endl;
+        return(0);
+    } else
+        seteuid(usuario->pw_uid);
 
-    qDebug()<<"lo primero que se te ocurra";
-    // Cambiar el grupo efectivo del proceso a 'midemonio'
-    group* group = getgrnam("recd");
-    setegid(group->gr_gid);
-
-    //Enviar un mensaje al demonio syslog
-    syslog(LOG_NOTICE, "Demonio iniciado con éxito\n");
+    // Cambiar el grupo efectivo del proceso
+    group* grupo = getgrnam("migue");    // GRUPO PROVISIONAL!
+    if (!grupo) {
+        std::cerr << "No existe el grupo en el sistema." << std::endl;
+        return(0);
+    } else
+        setegid(grupo->gr_gid);
 
 
     /****************/
@@ -106,11 +111,19 @@ int main(int argc, char *argv[]) {
     /**************/
 
     // Archivo que contiene identificador de proceso del demonio
-    QFile file("/var/run/recd.pid");
-    QTextStream out(&file);
-    out << pid;
-    file.close();
+    //QFile file("/var/run/recd.pid");
+    //QTextStream out(&file);
+    //out << pid;
+    //file.close();
 
+
+
+
+
+    // Cuando el demonio termine, cerrar la conexión con
+    // el servicio syslog
+    syslog(LOG_NOTICE, "Demonio REC finalizado con éxito");
+    closelog();
 
     return a.exec();
 }
